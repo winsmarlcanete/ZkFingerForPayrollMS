c09f1bc92c39d8659cb55679036f48587a749035
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-06-09 08:51:03 +0800

    working retrieval of employees from DB

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package src;
+
+import src.entity.Employee;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<Employee> emplist = EmployeeBiometric.retrieveAllEmployee();
+        for (Employee employee : emplist) {
+            System.out.println(employee.getLast_name());
+        }
+    }
+}
